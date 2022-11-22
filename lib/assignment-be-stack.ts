941cbfceb83a9e76cdbd7f94490bc5c1a72578fa
박상훈 <107174181+SangHoonPark1@users.noreply.github.com>
2022-11-22 21:56:44 +0900

    feat: 테스트용 lambda 생성
    테스트용 `hello-lambda-function`를 생성합니다
    lambda를 실행하여 hello wolrd를 출력합니다
    
    커밋할 변경 사항:
        lib/assignment-be-stack.ts
        package.json

--- lib/assignment-be-stack.ts
+++ lib/assignment-be-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class AssignmentBeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /**
+     * Lambda
+     * */
+    const code_path = "../lambda"
+
+    const helloLambda = new NodejsFunction(this, 'helloLambda', {
+      functionName: "hello-lambda-function",
+      entry: path.resolve(__dirname, code_path, 'hello.ts'),
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_16_X
+    })
+
   }
 }
