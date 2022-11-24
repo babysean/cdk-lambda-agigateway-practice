95e8aaab36eb4b1d164045873a5ca24d8c98fc4f
SangHoonPark1 <parksh1@focusmediakorea.com>
2022-11-24 20:56:08 +0900

    feat: DynamoDB Stack 정의
    DynamoDB의 Stack을 정의합니다
        tableName: user
        partitionKey: email
        billingMode: PAY_PER_REQUEST
    
    커밋할 변경 사항:
        lib/assignment-be-stack.ts

--- lib/assignment-be-stack.ts
+++ lib/assignment-be-stack.ts
@@ -3,10 +3,22 @@ import { Construct } from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "path";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class AssignmentBeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /**
+     * DynamoDB
+     * */
+
+    // DynamoDB Table 생성
+    const dynamoDB = new dynamodb.Table(this, 'assignment-dynamodb', {
+      tableName: 'user',
+      partitionKey: { name: 'email', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
 
     /**
      * Lambda
